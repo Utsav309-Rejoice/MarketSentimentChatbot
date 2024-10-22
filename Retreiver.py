cf026c1a1eeef255ba07b51904739e0487234562
Utsav309-Rejoice <utsav309.rejoice@gmail.com>
2024-10-22 10:45:51 +0530

    Update Retreiver.py

--- Retreiver.py
+++ Retreiver.py
@@ -1,6 +1,7 @@
 import os
 import pickle
 import json
+import streamlit as st
 from llama_index.llms.openai import OpenAI
 from llama_index.core.agent import ReActAgent
 from llama_index.core.tools import FunctionTool
